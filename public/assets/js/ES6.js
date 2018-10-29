a1d7923e39dcd450f693bda87a42408a83677938
Alex <jhon.1620@live.com>
2018-10-29 18:09:21 -0500

    fix(ES6): class

--- public/assets/js/ES6.js
+++ public/assets/js/ES6.js
@@ -56,56 +56,112 @@ class Modal {
     constructor() {
 
     }
-    
+
     static showModal() {
-        console.log('show')
+        // var texto = contenido.replace(/<[^>]*>?/g, '')
+        $('.modal').css({ 'display': 'block' });
     }
 
     static hideModal() {
-        console.log('hide')
+        $('.modal__close').on('click', function () {
+            $('.modal').css({ 'display': 'none' });
+        })
     }
 
-    catchDom() {
-        
+    catchDom1() {
+        console.log('catchDom1dsaffd')
     }
-
-    init(){
-
-    }
-
-}
-
-let MODAL_ = new Modal();
-
-MODAL_.showModal();
-
-Modal.catchDom();
-
-
-class ShowUsers{
-
-    constructor(){
-
-    }
-
-    events(){
-        $('.grid__btn-add').on('click')
-    }
-
-
-    showModalAddUser(){
-        Modal.showModal();
-    }
-
-    showModalUpdateUser() {
-        Modal.showModal();
-    }
-
-    catchDom() {
-        console.log('hide')
-    }
-
 }
 
 
-alert(`Probando rebase y merge`);
+class Listado {
+    constructor() {
+
+        this.name = 'Juan';
+        this.btnAdd = this.btnAdd ? this.btnAdd : '.grid__btn-add ';
+        this.btnUpdate = '.grid__btn-update';
+        this.btnDelete = '.grid__btn-delete';
+        this.template = 'template-Form';
+        this.container = '.modal__center';
+
+        this.dom = {};
+
+        this.catchDom();
+        this.events();
+    }
+
+    catchDom() {
+        this.dom.btnAdd = $(this.btnAdd);
+        this.dom.btnUpdate = $(this.btnUpdate);
+        this.dom.btnDelete = $(this.btnDelete);
+        this.dom.template = $(this.template);
+        this.dom.container = $(this.container);
+        console.log(this.template);
+    }
+
+    events() {
+
+        console.log(this.dom)
+
+        this.dom.btnAdd.on('click', () => {
+            this.add()
+        })
+        this.dom.btnUpdate.on('click', () => {
+            this.update()
+        })
+        this.dom.btnDelete.on('click', this.delete)
+    }
+
+    add() {
+        Modal.showModal();
+
+        let html = this.dom.template.html();
+
+        let tmp = _.template(html);
+        let compiled = tmp({ Title: 'Add User', Button: 'Add' })
+
+        this.dom.container.html(compiled)
+
+        Modal.hideModal();
+    }
+
+    update() {
+        console.log('Entrooooo')
+        Modal.showModal();
+        let html = document.getElementById(this.template).innerHTML;
+
+        console.log('html', html)
+        let tmp = _.template(html);
+        let compiled = tmp({ Title: 'Update User', Button: 'Update' })
+
+        this.dom.container.html(compiled)
+
+        Modal.hideModal();
+    }
+
+    delete() {
+        console.log('eliminando');
+    }
+}
+
+
+
+let lista = new Listado();
+
+
+// $(document).ready(
+//     $('#inp').change(function (e) {
+//         console.log(e.target.value);
+//     })
+// )
+
+
+let search = ""
+$('#inp').keypress(function (e) {
+    const val = e.key;
+    search = search + val;
+    console.log(e.target.value)
+})
+
+
+
