4575a8edc9241f5225db10b07bb593477e42735f
Alex <jhon.1620@live.com>
2018-11-06 18:38:49 -0500

    feat(ES6): ordenando modulo js

---
+++ public/assets/js/index/listUser.js
@@ -0,0 +1,76 @@
+class ListUser {
+
+    constructor() {
+
+        this.parent = '.js-listUser'
+        this.btnAdd = '.js-btn-add'
+        this.btnUpdate = '.js-btn-update'
+        this.btnDelete = '.js-btn-delete'
+        this.template = 'template-Form'
+        this.container = '.js-modal-center'
+
+        this.dom = {};
+
+        this.catchDom();
+        this.events();
+    }
+
+    catchDom() {
+        this.dom.parent = $(this.parent);
+        this.dom.btnAdd = $(this.btnAdd, this.dom.parent);
+        this.dom.btnUpdate = $(this.btnUpdate, this.dom.parent);
+        this.dom.btnDelete = $(this.btnDelete, this.dom.parent);
+        this.dom.template = $(this.template);
+        this.dom.container = $(this.container);
+    }
+
+    events() {
+
+        this.dom.btnAdd.on('click', () => {
+            this.addUser()
+        })
+        this.dom.btnUpdate.on('click', (e) => {
+            this.updateUser(e)
+        })
+        this.dom.btnDelete.on('click', (e) => {
+            this.deleteUser(e)
+        })
+    }
+
+    addUser() {
+
+        modal.showModal();
+
+        let html = document.getElementById(this.template).innerHTML;
+
+        let tmp = _.template(html);
+        let compiled = tmp({ Title: 'Add User', Button: 'Add' })
+
+        this.dom.container.html(compiled)
+
+        new SetUser(undefined);
+    }
+
+    updateUser(e) {
+
+        modal.showModal();
+
+        let html = document.getElementById(this.template).innerHTML;
+
+        let tmp = _.template(html);
+        let compiled = tmp({ Title: 'Update User', Button: 'Update' })
+
+        this.dom.container.html(compiled)
+
+        let id = $(e.target).attr('data-id')
+
+        new SetUser(id);
+    }
+
+    deleteUser(e) {
+
+        let id = $(e.target).attr('data-id')
+
+        new DeleteUser(id);
+    }
+}
---
+++ public/assets/js/index/modal.js
@@ -0,0 +1,38 @@
+class Modal {
+
+    constructor() {
+
+        this.parent = '.js-modal'
+        this.modalChild = '.js-modal-wrap'
+        this.btnClose = '.js-modal-close'
+
+        this.dom = {}
+
+        this.catchDom();
+    }
+
+    catchDom() {
+        this.dom.parent = $(this.parent)
+        this.dom.modalChild = $(this.modalChild, this.dom.parent)
+        this.dom.btnClose = $(this.btnClose, this.dom.parent)
+    }
+
+    events() {
+        this.dom.modalChild.on('click', this.hideModal)
+        this.dom.btnClose.on('click', this.hideModal)
+    }
+
+    showModal() {
+        this.dom.parent.show();
+    }
+
+    hideModal(e) {
+        if (e.target != this)
+            return
+        $('.js-modal').hide();
+    }
+
+    closeModal() {
+        this.dom.parent.hide();
+    }
+}
